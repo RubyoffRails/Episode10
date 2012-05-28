b1fab04bbb076c97d4d1377dda27124c2f785586
Peter Aronoff <telemachus@arpinum.org>
2012-05-28 10:37:25 -0400

    Eagle assignment
    I'm not sure if this code is awesome or horrific or both. It definitely
    scares me some.

---
+++ lib/meta/terminator.rb
@@ -0,0 +1,11 @@
+class GoodTerminator
+  include Terminatorable
+  likes_to_protect [:john_connor, :sarah_connor]
+  likes_to_destroy # defaults to an empty array, no args needed
+end
+
+class BadTerminator
+	include Terminatorable
+	likes_to_protect # defaults to an empty array, no args needed
+	likes_to_destroy [:john_connor, :sarah_connor]
+end
