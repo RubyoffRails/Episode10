e3c96d3571665a1e4c20faa0a7f409b5bbc50768
meltar <meltar@gmail.com>
2013-05-03 16:45:24 -0400

    Tiger and eagle complete

--- terminator.rb
+++ terminator.rb
@@ -2,7 +2,6 @@ require "./terminatorable"
 
 class Terminator
   include Terminatorable
-  likes_to_protect [:john_connor, :sarah_connor]
   good
 
   def protects?(name)
@@ -10,3 +9,12 @@ class Terminator
   end
 end
 
+class GoodTerminator < Terminator
+  likes_to_protect [:john_connor, :sarah_connor]
+  likes_to_destroy []
+end
+
+class BadTerminator < Terminator
+  likes_to_protect []
+  likes_to_destroy [:john_connor, :sarah_connor]
+end
--- terminatorable.rb
+++ terminatorable.rb
@@ -2,11 +2,17 @@ module Terminatorable
 
   module ClassMethods
     def likes_to_protect(people=[])
-      ["destroy", "protect"].each do |mission_type|
-        people.each do |person|
-          define_method "#{mission_type}_#{person}!" do
-            @current_mission = "#{mission_type}: #{person}"
-          end
+      people.each do |person|
+        define_method "protect_#{person}!" do
+          @current_mission = "protect: #{person}"
+        end
+      end
+    end
+
+    def likes_to_destroy(people=[])
+      people.each do |person|
+        define_method "destroy_#{person}!" do
+          @current_mission = "destroy: #{person}"
         end
       end
     end
